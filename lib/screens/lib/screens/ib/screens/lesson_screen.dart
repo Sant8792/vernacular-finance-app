da3b06fa1a2420c6e0de4d772199021c49f81739
Sant8792 <119758603+Sant8792@users.noreply.github.com>
2025-07-06 17:53:20 +0530

    lesson_screen.dart

---
+++ lib/screens/lib/screens/ib/screens/lesson_screen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class LessonScreen extends StatelessWidget {
+  final Map lesson;
+
+  LessonScreen(this.lesson);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(lesson['title'])),
+      body: Padding(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              lesson['description'],
+              style: TextStyle(fontSize: 18),
+            ),
+            SizedBox(height: 20),
+            Placeholder(fallbackHeight: 200, fallbackWidth: double.infinity),
+            // You can replace Placeholder with Video/Audio player later
+          ],
+        ),
+      ),
+    );
+  }
+}
