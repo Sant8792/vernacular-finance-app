f9136e2e5ba0afee3a6bde83a882de48794d126e
Sant8792 <119758603+Sant8792@users.noreply.github.com>
2025-07-06 17:56:55 +0530

    Create lessons.dart

---
+++ lessons.dart
@@ -0,0 +1,10 @@
+final lessons = [
+  {
+    'title': 'what_is_upi',
+    'description': 'Unified Payments Interface explained...',
+  },
+  {
+    'title': 'how_to_save_money',
+    'description': 'Smart ways to save small amounts...',
+  },
+];
